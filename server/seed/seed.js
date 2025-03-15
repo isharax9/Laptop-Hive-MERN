f910c555837da5c20a4480498a1fd3caa78ff21f
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2025-03-15 12:13:09 +0530

    seed modified

--- server/seed/seed.js
+++ server/seed/seed.js
@@ -3,10 +3,7 @@ const {seedCategory} = require("./Category")
 const {seedProduct} = require("./Product")
 const {seedUser} = require("./User")
 const {seedAddress} = require("./Address")
-const {seedWishlist} = require("./Wishlist")
-const {seedCart} = require("./Cart")
 const {seedReview} = require("./Review")
-const {seedOrder} = require("./Order")
 const {DBConnection} = require("../database/DBConnection")
 const mongoose = require("mongoose");
 
@@ -20,10 +17,7 @@ const seedData = async () => {
         await seedProduct()
         await seedUser()
         await seedAddress()
-        // await seedWishlist()
-        // await seedCart()
         await seedReview()
-        // await seedOrder()
 
         console.log('\x1b[34mSeed completed successfully\x1b[0m');
     } catch (error) {
